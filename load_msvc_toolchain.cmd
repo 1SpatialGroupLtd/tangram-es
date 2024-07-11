c6d2f5b8453060c14abee34e6944579c7511ec9c
Vilmos Kulcsar <vilmos.kulcsar@1spatial.com>
2024-07-11 11:45:40 +0100

    load_msvc_toolchain.cmd now can accept external vcvars path

--- load_msvc_toolchain.cmd
+++ load_msvc_toolchain.cmd
@@ -1,5 +1,9 @@
 @echo off
-if "%PATH%"=="%PATH:\2022\Professional\VC\Tools\MSVC\=%" (
-	call "C:\Program Files\Microsoft Visual Studio\2022\Professional\VC\Auxiliary\Build\vcvars64.bat"
+
+if "%PATH%"=="%PATH:\Tools\MSVC\=%" (
+	if not defined VC_VARS_PATH (
+		call "C:\Program Files\Microsoft Visual Studio\2022\Professional\VC\Auxiliary\Build\vcvars64.bat"
+	) else ( call %VC_VARS_PATH% )
 )
+
 exit /b 0
